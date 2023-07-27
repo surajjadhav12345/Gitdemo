6186410fac14587dcdbfc4d1d780dab65907c3af
Suraj <suraj.jadhav0745@gmail.com>
2023-07-27 20:03:47 +0530

    third commit

--- src/Demo/sampl1.java
+++ src/Demo/sampl1.java
@@ -4,6 +4,9 @@ public class sampl1 {//method body open
 	public static void main(String[] args) {
 		float a=12.45f;
 		System.out.println(a);
+		System.out.println(b);
+		System.out.println(b);
+		
 
 	
 	}
